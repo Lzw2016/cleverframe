0abd5f3b22e4947073b6f7c35840561a48341908
Lzw2016 <1183409807@qq.com>
2016-06-18 12:47:05 +0800

    若干优化、改造

---
+++ clever-core/src/main/java/org/cleverframe/core/service/EhCacheTemplateService.java
@@ -0,0 +1,14 @@
+package org.cleverframe.core.service;
+
+import org.cleverframe.core.freemarker.ITemplateService;
+
+/**
+ * 模版数据Service服务实现类，使用EhCache缓存数据
+ * <p/>
+ * 作者：LiZW <br/>
+ * 创建时间：2016-6-18 12:40 <br/>
+ */
+
+public class EhCacheTemplateService implements ITemplateService {
+
+}
