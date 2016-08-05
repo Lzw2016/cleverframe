7cc43ca3e91914129019c2ebb36324cde6b6c156
Lzw2016 <1183409807@qq.com>
2016-08-06 00:19:34 +0800

    若干修改

---
+++ clever-quartz/src/main/java/org/cleverframe/quartz/controller/QrtzSchedulerLogController.java
@@ -0,0 +1,23 @@
+package org.cleverframe.quartz.controller;
+
+import org.cleverframe.common.controller.BaseController;
+import org.cleverframe.quartz.QuartzBeanNames;
+import org.cleverframe.quartz.service.QrtzSchedulerLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 作者：LiZW <br/>
+ * 创建时间：2016-8-5 11:09 <br/>
+ */
+@SuppressWarnings("MVCPathVariableInspection")
+@Controller
+@RequestMapping(value = "/${mvcPath}/quartz/schedulerlog")
+public class QrtzSchedulerLogController extends BaseController {
+
+    @Autowired
+    @Qualifier(QuartzBeanNames.QrtzSchedulerLogService)
+    private QrtzSchedulerLogService qrtzSchedulerLogService;
+}
